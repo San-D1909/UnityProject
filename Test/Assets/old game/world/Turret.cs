8bf90b46a6dd93729bed86b3a6592e2facf2f755
Remco-Bruinsma <82671724+Remco-Bruinsma@users.noreply.github.com>
2021-12-29 22:07:09 +0100

    i changed turret

--- Test/Assets/old game/world/Turret.cs
+++ Test/Assets/old game/world/Turret.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Turret : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Transform Target;
+    public Transform Target;
     [SerializeField] private float range = 15f;
     [SerializeField] private string enemyTag = "Enemy";
+    [SerializeField] private Transform baseTurret;
 
     private void Start()
     {
@@ -26,7 +28,7 @@ public class Turret : MonoBehaviour
             {
                 shortestDistance = distanceToEnemy;
                 nearestEnemy = enemy;
-            }
+            }   
         }
         if (nearestEnemy != null && shortestDistance <= range)
         {
@@ -39,18 +41,23 @@ public class Turret : MonoBehaviour
     }
     private void Update()
     {
-        if (Target=null)
+        if (Target = null)
         {
-
             return;
         }
+            
+
         Vector3 dir = Target.position - transform.position;
-        /*Quaternion lookRotation = new Quaternion();*/
+        Quaternion lookrotation = Quaternion.LookRotation(dir);
+        Vector3 turretrotation = lookrotation.eulerAngles;
+        baseTurret.rotation = Quaternion.Euler(0f, turretrotation.y, 0f);
+
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, range);
+        
     }
 
 }
